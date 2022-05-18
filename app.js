2bb7577074fe8b680602fb8cba6cda2c296b90ea
Arrombado <rafaoc7232@hotmail.com>
2022-05-18 15:23:28 -0300

    feat(): add top css style for animation expansion effect

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const headline = document.querySelector('.headline');
 
 const tl = gsap.timeline()
 
-tl.fromTo(hero,1,{height:'0%'},{height: '100%', ease: Power2.easeInOut})
+tl.fromTo(hero,1,{height:'0%',top: '50%'},{height: '100%',top: '0%', ease: Power2.easeInOut})
 .fromTo(slider, 1.2, {x: "-100%"}, {x:"0%", ease:Power2.easeInOut})
 .fromTo(logo, 0.5, {opacity: 0, x:30},{opacity: 1, x:0},"-=0.5")
 .fromTo(hamburger, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
